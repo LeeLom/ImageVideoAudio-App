14f1399f62e0fd0415bdad2681b93475d4ed6aaa
LeeLom <771704330@qq.com>
2016-03-30 09:33:00 +0800

    Initial Commit

---
+++ GraduateDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GraduateDemo
+//
+//  Created by LeeLom on 16/3/30.
+//  Copyright © 2016年 LeeLom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
